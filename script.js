e4274389ba5ef530052b990f3cfc488894e3223d
BOGGALA YASWANTH REDDY <93768814+yash2700@users.noreply.github.com>
2023-05-29 13:37:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+  }
+
+function stop() {
+  var audio = document.getElementById("audio");
+  audio.pause();
+  audio.currentTime = 0;
+}
